26ec99565dd7bc8169d1459e19ac2ecc2c593921
NelsonFrancis <developer.nelson22@gmail.com>
2025-09-26 17:20:14 +0400

    frontend done

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,8 +1,60 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { StoreContext } from '../context/StoreContext'
 
 const PlaceOrder = () => {
+  const {getTotalCartAmount} = useContext(StoreContext);
+
   return (
-    <div>PlaceOrder</div>
+    <>
+    {
+        getTotalCartAmount() === 0 
+        ? <p className='no-items'>Oops!! No items in cart</p> 
+        : <>
+    <form className='place-order'>
+      <div className="place-order-left">
+        <p className='title'>Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder='First name'/>
+          <input type="text" placeholder='Last name' />
+        </div>
+        <input type="text" placeholder='Email id'/>
+        <input type="text" placeholder='Street' />
+        <div className="multi-fields">
+          <input type="text" placeholder='City'/>
+          <input type="text" placeholder='State' />
+        </div>
+        <div className="multi-fields">
+          <input type="text" placeholder='Zipcode'/>
+          <input type="text" placeholder='Country' />
+        </div>
+        <input type='text' placeholder='Phone' />
+      </div>
+      <div className="place-order-right">
+        <div className="cart-total">
+          <h2>Cart Totals</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery fee</p>
+              <p>${2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>${getTotalCartAmount() + 2}</b>
+            </div>
+          </div>
+          <button>Proceed to payment</button>
+        </div>
+      </div>
+    </form>
+    </>
+    }
+    </>
   )
 }
 
